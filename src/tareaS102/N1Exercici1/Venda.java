f0b3610d1d7abd8dfb3271a3f8a1c69f81a84cab
Jordi Cid <jordicid1@gmail.com>
2023-11-07 09:40:50 +0100

    Sprint 1 Tarea S102 Excepciones Nivel 1 Exercici1 vuelto a corregir

--- src/tareaS102/N1Exercici1/Venda.java
+++ src/tareaS102/N1Exercici1/Venda.java
@@ -8,27 +8,21 @@ public class Venda {
 	ArrayList<Producte> productList = new ArrayList<Producte>();
 	float preuTotal;
 
-
 	public void addProducte(Producte p) {
 		productList.add(p);
 	}
-	public void calcularTotal() throws VendaBuidaException
-	{
-		
-		try {
-			if (productList.isEmpty()) {
-				throw new VendaBuidaException(msg);
-			}
-			//System.out.println(productList.get(1).nom);
-			
-			for (Producte producte : productList) {
-				System.out.println(producte.nom + " | " + producte.preu);
-				preuTotal += producte.preu;
-			}
 
-		} catch (IndexOutOfBoundsException e) {
-			// TODO: handle exception
+	public void calcularTotal() throws VendaBuidaException {
+
+		if (productList.isEmpty()) {
 			throw new VendaBuidaException(msg);
 		}
+		// System.out.println(productList.get(1).nom);
+
+		for (Producte producte : productList) {
+			System.out.println(producte.nom + " | " + producte.preu);
+			preuTotal += producte.preu;
+		}
+
 	}
 }
--- src/tareaS102/N1Exercici1/main.java
+++ src/tareaS102/N1Exercici1/main.java
@@ -5,33 +5,33 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Venda v = new Venda();
+		// Esto debería generar la excepción ya que no hemos introducido ningún producto
+		// en la venta
 		try {
-			// Esto debería generar la excepción ya que no hemos introducido ningún producto
-			// en la venta
-			try {
-				v.calcularTotal();
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
+			v.calcularTotal();
+		} catch (VendaBuidaException e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			// con la linea anterior comentada la excepción no debería darse
+			System.out.println(v.preuTotal);
+			for (int i = 1; i <= 10; i++) {
+				Producte p = new Producte();
+				p.nom = "Producte" + i;
+				p.preu = i;
+				v.addProducte(p);
 			}
-			try {
-				// con la linea anterior comentada la excepción no debería darse
-				System.out.println(v.preuTotal);
-				for (int i = 1; i <= 10; i++) {
-					Producte p = new Producte();
-					p.nom = "Producte" + i;
-					p.preu = i;
-					v.addProducte(p);
-				}
-				v.calcularTotal();
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
-			System.out.println("Preu Total Venda " + v.preuTotal);
-			// esto provoca el IndexOutOfBoundException y se muestra un mensaje distinto al
+			v.calcularTotal();
+		} catch (VendaBuidaException e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println("Preu Total Venda " + v.preuTotal);
+		// esto provoca el IndexOutOfBoundException y se muestra un mensaje distinto al
+		try {
 			// no pasar por el método CalcularTotal()
 			Venda v2 = new Venda();
 			System.out.println(v2.productList.get(1).nom);
-		} catch (Exception e) {
+		} catch (IndexOutOfBoundsException e) {
 			System.out.println(e.getMessage());
 		}
 	}
