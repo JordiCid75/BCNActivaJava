592af51c6bd28095117d0c06fade98d31a49a00d
Jordi Cid <jordicid1@gmail.com>
2023-11-10 18:00:15 +0100

    Sprint 2 Tarea S202 todos los niveles

---
+++ src/Sprint2/TareaS202/Consultas_Universidad.sql
@@ -0,0 +1,66 @@
+use `Universidad`;
+
+/*SQL #1*/
+Select apellido1, apellido2, nombre from persona where tipo = "alumno" order by apellido1, apellido2, nombre;
+/*SQL #2*/
+Select apellido1, apellido2, nombre from persona where tipo = "alumno" and telefono is null;
+/*SQL #3*/
+Select apellido1, apellido2, nombre from persona where tipo = "alumno" and year(fecha_nacimiento) = 1999;
+/*SQL #4*/
+Select apellido1, apellido2, nombre from persona where tipo = "profesor" and telefono is null and nif like '%K';
+/*SQL #5*/
+Select * from asignatura where cuatrimestre = 1 and curso = 3 and id_grado = 7;
+/*SQL #6*/
+Select pe.apellido1, pe.apellido2, pe.nombre, d.nombre from profesor p  inner join persona pe on pe.id = p.id_profesor inner join departamento d on p.id_departamento = d.id order by pe.apellido1, pe.apellido2, pe.nombre;
+/*SQL #7*/
+select a.nombre, c.anyo_inicio, c.anyo_fin from alumno_se_matricula_asignatura asma inner join persona p on p.id = asma.id_alumno inner join asignatura a on a.id = asma.id_asignatura inner join curso_escolar c on c.id = asma.id_curso_escolar where nif = '26902806M';
+/*SQL #8*/
+select d.nombre from departamento d inner join profesor p on p.id_departamento = d.id inner join asignatura a on a.id_profesor = p.id_profesor where a.id_grado = 4 group by d.nombre;
+/*SQL #9*/
+select p.apellido1, p.apellido2, p.nombre, p.nif from alumno_se_matricula_asignatura asma inner join persona p on p.id = asma.id_alumno where asma.id_curso_escolar = 5 group by p.apellido1, p.apellido2, p.nombre, p.nif;
+
+/*SQL LEFT o RIGHT JOIN*/
+/*SQL #1*/
+select  d.nombre, pe.apellido1, pe.apellido2, pe.nombre from persona pe left join profesor p on p.id_profesor = pe.id right join departamento d on d.id = p.id_departamento where pe.tipo = "Profesor" order by d.nombre, pe.apellido1, pe.apellido2, pe.nombre;
+
+/*SQL #2*/
+select * from persona left join profesor on profesor.id_profesor = persona.id where  tipo = "Profesor" and profesor.id_profesor is null;
+
+/*SQL #3*/
+select d.* from departamento d left join profesor p on d.id = p.id_departamento where p.id_departamento is null;
+
+/*SQL #4*/
+select p.* from persona p left join asignatura a on a.id_profesor = p.id where a.id_profesor is null and p.tipo = "Profesor";
+
+/*SQL #5*/
+select a.* from asignatura a left join persona p on a.id_profesor = p.id where a.id_profesor is null;
+
+/*SQL #6*/
+select d.id, d.nombre from profesor p
+inner join departamento d on d.id = p.id_departamento
+left join asignatura a on a.id_profesor = p.id_profesor where a.id is null group by d.id, d.nombre;
+
+/*Consultas resumen*/
+/*SQL #1*/
+select count(*) from persona where tipo = "Alumno";
+/*SQL #2*/
+select count(*) from persona where tipo = "Alumno" and year(fecha_nacimiento) = 1999;
+/*SQL #3*/
+select d.nombre, count(p.id_profesor) as numProfesores from profesor p inner join departamento d on d.id = p.id_departamento group by d.nombre order by numProfesores;
+/*SQL #4*/
+select d.nombre, count(p.id_profesor) as numProfesores from profesor p right join departamento d on d.id = p.id_departamento group by d.nombre order by numProfesores;
+/*SQL #5*/
+select g.nombre, count(a.id) as numAsignaturas from grado g left join asignatura a on a.id_grado = g.id group by g.nombre order by numAsignaturas;
+/*SQL #6*/
+select g.nombre, count(a.id) as numAsignaturas from grado g left join asignatura a on a.id_grado = g.id group by g.nombre having count(a.id) > 40 order by numAsignaturas;
+/*SQL #7*/
+select g.nombre, a.tipo, sum(a.creditos) as SumaCreditos from grado g inner join asignatura a on a.id_grado = g.id group by g.nombre, a.tipo;
+/*SQL #8*/
+select c.anyo_inicio, count(asma.id_alumno) as NumeroAlumnos from curso_escolar c left join alumno_se_matricula_asignatura asma on asma.id_curso_escolar = c.id group by c.anyo_inicio;
+/*SQL #9*/
+select pe.id, pe.nombre, pe.apellido1, pe.apellido2, count(a.id) as NumAsignaturas from persona pe left join asignatura a on a.id_profesor = pe.id where pe.tipo = "Profesor" group by pe.id, pe.nombre, pe.apellido1, pe.apellido2 order by NumAsignaturas;
+/*SQL #10*/
+select pe.* from persona pe where pe.fecha_nacimiento = (select max(fecha_nacimiento) from persona where tipo = "Alumno") and pe.tipo = "Alumno";
+/*SQL #11*/
+select pe.id, pe.nombre, pe.apellido1, pe.apellido2, count(a.id) as NumAsignaturas from persona pe left join asignatura a on a.id_profesor = pe.id where pe.tipo = "Profesor" group by pe.id, pe.nombre, pe.apellido1, pe.apellido2 having count(a.id) = 0 order by NumAsignaturas;
+
---
+++ src/Sprint2/TareaS202/Conultas_Tienda.sql
@@ -0,0 +1,83 @@
+USE tienda;
+/*SQL #1*/
+select nombre from producto;
+/*SQL #2*/
+Select nombre, precio from producto;
+/*SQL #3*/
+select * from producto;
+/*SQL #4*/
+select nombre, precio as PrecioEUR, (precio * 1.07) as PrecioUSD from producto;
+/*SQL #5*/
+select nombre as 'Nom de Producto', precio as euros, (precio * 1.07) as dolars from producto;
+/*SQL #6*/
+select upper(nombre), precio from producto; 
+/*SQL #7*/
+select lower(nombre), precio from producto; 
+/*SQL #8*/
+select nombre, upper(left(nombre,2)) as ABR_Fabricante from fabricante;
+/*SQL #9*/
+select nombre, round(precio) from producto; 
+/*SQL #10*/
+select nombre, truncate(precio,0) from producto; 
+/*SQL #11*/
+select f.codigo from fabricante f inner join producto p on p.codigo_fabricante = f.codigo;
+/*SQL #12*/
+select f.codigo from fabricante f inner join producto p on p.codigo_fabricante = f.codigo group by f.codigo;
+/*SQL #13*/
+select nombre from fabricante order by nombre ASC;
+/*SQL #14*/
+select nombre from fabricante order by nombre DESC;
+/*SQL #15*/
+select nombre from producto order by nombre ASC, precio DESC;
+/*SQL #16*/
+select * from fabricante limit 5;
+/*SQL #17*/
+select * from fabricante limit 3,2;
+/*SQL #18*/
+select nombre, precio from producto order by precio ASC limit 1;
+/*SQL #19*/
+select nombre, precio from producto order by precio DESC limit 1;
+/*SQL #20*/
+select nombre, codigo_fabricante from producto where codigo_fabricante = 2;
+/*SQL #21*/
+select p.nombre, p.precio, f.nombre from producto p inner join fabricante f on p.codigo_fabricante = f.codigo;
+/*SQL #22*/
+select p.nombre, p.precio, f.nombre from producto p inner join fabricante f on p.codigo_fabricante = f.codigo order by f.nombre ASC;
+/*SQL #23*/
+select p.nombre, p.precio, p.codigo_fabricante, f.nombre from producto p left join fabricante f on p.codigo_fabricante = f.codigo;
+/*SQL #24*/
+select p.nombre, p.precio, f.nombre from producto p inner join fabricante f on p.codigo_fabricante = f.codigo order by p.precio limit 1;
+/*SQL #25*/
+select p.nombre, p.precio, f.nombre from producto p inner join fabricante f on p.codigo_fabricante = f.codigo order by p.precio DESC limit 1;
+/*SQL #26*/
+select p.* from producto p inner join fabricante f on p.codigo_fabricante = f.codigo where f.nombre = "Lenovo";
+/*SQL #27*/
+select p.* from producto p inner join fabricante f on p.codigo_fabricante = f.codigo where f.nombre = "Lenovo" and p.precio > 200;
+/*SQL #28*/
+select p.* from producto p inner join fabricante f on p.codigo_fabricante = f.codigo where f.nombre = "Asus" or f.nombre = "Hewlett-Packard" or f.nombre = "Seagate";
+/*SQL #29*/
+select p.* from producto p inner join fabricante f on p.codigo_fabricante = f.codigo where f.nombre in("Asus", "Hewlett-Packard", "Seagate");
+/*SQL #30*/
+select p.nombre, p.precio from producto p inner join fabricante f on p.codigo_fabricante = f.codigo where f.nombre like '%e';
+/*SQL #31*/
+select p.nombre, p.precio from producto p inner join fabricante f on p.codigo_fabricante = f.codigo where f.nombre like '%w%';
+/*SQL #32*/
+select p.nombre, p.precio, f.nombre from producto p inner join fabricante f on p.codigo_fabricante = f.codigo where p.precio >= 180 order by p.precio DESC, p.nombre;
+/*SQL #33*/
+select f.codigo, f.nombre from fabricante f where f.codigo in (select codigo_fabricante from producto group by codigo_fabricante);
+/*SQL #34*/
+select f.*, p.* from fabricante f left join producto p on p.codigo_fabricante = f.codigo;
+/*SQL #35*/
+select f.*, p.* from fabricante f left join producto p on p.codigo_fabricante = f.codigo where p.codigo_fabricante is null;
+/*SQL #36*/
+select p.* from producto p where p.codigo_fabricante in(select codigo from fabricante where nombre = "Lenovo");
+/*SQL #37*/
+select p.nombre from producto p where p.precio = (select max(precio) from producto p where p.codigo_fabricante = (Select codigo from fabricante where nombre = "Lenovo"));
+/*SQL #38*/
+select p.nombre from producto p inner join fabricante f on p.codigo_fabricante = f.codigo where f.nombre = "Lenovo" and p.precio = (select max(precio) from producto p where p.codigo_fabricante = f.codigo);
+/*SQL #39*/
+select p.nombre from producto p inner join fabricante f on p.codigo_fabricante = f.codigo where f.nombre = "Hewlett-Packard" and p.precio = (select min(precio) from producto p where p.codigo_fabricante = f.codigo);
+/*SQL #40*/
+select p.* from producto p where p.precio >= (select max(precio) from producto p where p.codigo_fabricante = (Select codigo from fabricante where nombre = "Lenovo"));
+/*SQL #41*/
+select p.* from producto p inner join fabricante f on p.codigo_fabricante = f.codigo where f.nombre = "Asus" and p.precio > (select avg(precio) from producto p where p.codigo_fabricante = f.codigo);
