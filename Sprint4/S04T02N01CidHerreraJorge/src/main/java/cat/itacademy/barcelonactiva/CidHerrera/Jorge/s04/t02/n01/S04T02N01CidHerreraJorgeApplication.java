60904ea537b4d46ad7ad34131ee2572bb4b5f947
Jordi Cid <jordicid1@gmail.com>
2024-01-30 15:54:10 +0100

    S4T02N1 update

---
+++ Sprint4/S04T02N01CidHerreraJorge/src/main/java/cat/itacademy/barcelonactiva/CidHerrera/Jorge/s04/t02/n01/S04T02N01CidHerreraJorgeApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.CidHerrera.Jorge.s04.t02.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N01CidHerreraJorgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N01CidHerreraJorgeApplication.class, args);
+	}
+
+}
